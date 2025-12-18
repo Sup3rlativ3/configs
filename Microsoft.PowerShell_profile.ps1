4d8db60b33de45ca11cc5408c53a81f2f2fbc3a1
Sup3rlativ3 <1555043+Sup3rlativ3@users.noreply.github.com>
2025-12-18 15:09:09 +1100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,40 +1,4 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Import-Module posh-git, Terminal-Icons
+Import-Module Terminal-Icons
 
-
-
-function Test-Administrator {
-    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
-    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-}
-
-function Test-ADCredential {
-    [CmdletBinding()]
-    param ( [string]$Username,
-        [string]$Password
-)
-    Param
-    (
-        [Parameter(Mandatory=$true)][string]$UserName,
-        [Parameter(Mandatory=$true)][string]$Password
-    )
-        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext('domain')
-        $DS.ValidateCredentials($UserName, $Password)
-    }
-
-function global:prompt {  # Multiple Write-Host commands with color
-    if(Test-Administrator){
-        Write-Host "[A]" -ForegroundColor White -BackgroundColor Red -NoNewline
-        Write-Host(" ") -NoNewline
-    }
-    Write-Host("[") -nonewline
-    Write-Host((Get-Date).ToShortTimeString()) -nonewline -foregroundcolor Red
-    Write-Host("] ") -nonewline
-    Write-Host([Environment]::UserName) -nonewline 
-    Write-Host("@") -NoNewline
-    Write-Host([Environment]::MachineName) -NoNewline
-    Write-Host(" ") -NoNewline
-    Write-Host($(Split-Path $pwd -Leaf)) -nonewline -foregroundcolor Blue
-    return " >"
-}
+$ProfilePath = Get-Item $Profile
+oh-my-posh --init --shell pwsh --config "$($ProfilePath.Directory)\SysOp.omp.json" | Invoke-Expression
